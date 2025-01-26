85d9ac74cd53504c766cd5664548e952cf03c185
Pawan Singh <119283159+Pawan09Singh@users.noreply.github.com>
2025-01-26 20:22:03 +0530

    Add files via upload

---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBox from '../../components/SearchBox/SearchBox'
+
+const Home = () => {
+  return (
+    <div className="home-container">
+       <SearchBox/>
+    </div>
+  )
+}
+
+export default Home
